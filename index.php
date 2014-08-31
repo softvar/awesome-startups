e0f245ca1d775a3251c9504bc66b1863490ddf65
softvar <varun2902@gmail.com>
2014-08-31 20:00:00 +0530

    beautify code, removed unused code

--- index.php
+++ index.php
@@ -20,24 +20,26 @@ function curl($url)
  * Append Newline `$times` times to the specfied `$str` string
  */
 
-function insertNewline($str, $times) {
-	for ($k = 0; $k < $times; $k++) {
-		$str .= PHP_EOL;
-	}
-	return $str;
+function insertNewline($str, $times)
+{
+    for ($k = 0; $k < $times; $k++) {
+        $str .= PHP_EOL;
+    }
+    return $str;
 }
 
 /*
  * Append text `$text` to the specified `$dest` destination string.
  */
 
-function appendText($dest, $text) {
-	$dest .= $text;
-	return $dest;
+function appendText($dest, $text)
+{
+    $dest .= $text;
+    return $dest;
 }
 
 /*
- *	Prepare README Content
+ *  Prepare README Content
  */
 $readmeOutput = file_get_contents('pre-readme-content.txt');
 $readmeOutput = insertNewline($readmeOutput, 2);
@@ -46,21 +48,19 @@ $readmeOutput = insertNewline($readmeOutput, 2);
 $make         = 'http://www.startupranking.com/top/';
 // get page contents
 $url          = curl($make);
-//echo $url;
 $regex        = "/<div class=\"name\"?>.*<a.*?>([^`]*?)<\/a><\/div>/";
-
 preg_match_all($regex, $url, $startupNames);
 
-$doc=new DOMDocument();
+$doc = new DOMDocument();
 @$doc->loadHTML($url);
-$xml=simplexml_import_dom($doc); // just to make xpath more simple
-$locations=$xml->xpath('//*[@class="f32"]//img');
+$xml       = simplexml_import_dom($doc); // just to make xpath more simple
+$locations = $xml->xpath('//*[@class="f32"]//img');
 
 foreach ($startupNames[0] as $key => $value) {
     $value = strtolower(strip_tags($value));
     $value = str_replace(' ', '-', $value);
     if ($value != '') {
-        $startupUrl = $BASE_URL . lcfirst($value);
+        $startupUrl   = $BASE_URL . lcfirst($value);
         $readmeOutput = appendText($readmeOutput, ($key + 1) . '. [' . $value . '](' . $startupUrl . ') - ' . $locations[$key]['title']);
         $readmeOutput = insertNewline($readmeOutput, 1);
     }
@@ -84,37 +84,37 @@ foreach ($listOfSupportedCountries['countries'] as $key => $value) {
 
 $postReadmeContent = insertNewline($postReadmeContent, 1);
 $postReadmeContent = appendText($postReadmeContent, file_get_contents('post-readme-content.txt'));
-$readmeOutput = appendText($readmeOutput, $postReadmeContent);
+$readmeOutput      = appendText($readmeOutput, $postReadmeContent);
 file_put_contents('README.md', $readmeOutput);
 
-for ($j = 0; $j < count($listOfSupportedCountries['countries']); $j++) { // $j < count($listOfSupportedCountries)
+for ($j = 0; $j < count($listOfSupportedCountries['countries']); $j++) {
+
     $hyphenSeparatedCountryName = $listOfSupportedCountries['countries'][$j];
-    //echo ucfirst($value);
     $splitCountryName           = explode("-", $hyphenSeparatedCountryName);
     $countryName                = ucfirst($splitCountryName[0]);
     for ($i = 1; $i < count($splitCountryName); $i++) {
         $countryName = $countryName . " " . ucfirst($splitCountryName[$i]);
     }
-    $countryOutput = '## ' . $countryName ;
+    $countryOutput = '## ' . $countryName;
     $countryOutput = insertNewline($countryOutput, 2);
-    $make         = 'http://www.startupranking.com/top/' . $hyphenSeparatedCountryName;
+    $make          = 'http://www.startupranking.com/top/' . $hyphenSeparatedCountryName;
     // get page contents
-    $url          = curl($make);
+    $url           = curl($make);
     //echo $url;
-    $regex        = "/<div class=\"name\"?>.*<a.*?>([^`]*?)<\/a><\/div>/";
+    $regex         = "/<div class=\"name\"?>.*<a.*?>([^`]*?)<\/a><\/div>/";
 
     preg_match_all($regex, $url, $startupNames);
     foreach ($startupNames[0] as $key => $value) {
         $value = strtolower(strip_tags($value));
         $value = str_replace(' ', '-', $value);
         if ($value != '') {
-            $startupUrl = $BASE_URL . lcfirst($value);
+            $startupUrl    = $BASE_URL . lcfirst($value);
             $countryOutput = appendText($countryOutput, ($key + 1) . '. [' . $value . '](' . $startupUrl . ')');
             $countryOutput = insertNewline($countryOutput, 1);
         }
     }
     $countryOutput = insertNewline($countryOutput, 1);
-    $filename = 'countries/' . $hyphenSeparatedCountryName . '.md';
+    $filename      = 'countries/' . $hyphenSeparatedCountryName . '.md';
     file_put_contents($filename, $countryOutput);
     $countryOutput = '';
 }
